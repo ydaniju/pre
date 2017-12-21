da3cca19f219b41a1714eb262d118c92d752b517
ydaniju <yusuf.daniju@andela.com>
2017-12-21 16:48:31 +0100

    flesh out the simple node part

--- setup.sh
+++ setup.sh
@@ -1,3 +1,22 @@
 #!/usr/bin/env bash
+DIRECTORY=UserManager
 
-git clone git@github.com:BolajiOlajide/UserManager.git
+if [ ! -d "$DIRECTORY" ]; then
+  git clone git@github.com:BolajiOlajide/UserManager.git
+fi
+
+if type node > /dev/null
+  then
+    echo ""
+    echo "node is installed, skipping..."
+  else
+    brew install node
+    # sudo apt-get update
+    # sudo apt-get install nodejs
+  fi
+
+cd $DIRECTORY
+
+npm install
+
+npm start
