3ea8e2a6e35f53c2633bbcf51d8f0582ad98515b
ydaniju <yusuf.daniju@andela.com>
2017-12-22 09:08:32 +0100

    dockerfile creation

--- setup.sh
+++ setup.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
+touch Dockerfile
+
+echo "
+FROM node:latest
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+COPY package.json /usr/src/app/
+RUN npm install
+COPY . /usr/src/app
+EXPOSE 3000
+CMD [ 'npm', 'start' ]
+" >> Dockerfile
+
 DIRECTORY=UserManager
 
 if [ ! -d "$DIRECTORY" ]; then
