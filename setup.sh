0512d30214a0210221bcf09748f6c09dbeb25fdf
ydaniju <yusuf.daniju@andela.com>
2017-12-23 15:18:53 +0100

    express server now working

--- setup.sh
+++ setup.sh
@@ -1,36 +1,56 @@
 #!/usr/bin/env bash
-touch Dockerfile
-
-echo "
-FROM node:latest
-RUN mkdir -p /usr/src/app
-WORKDIR /usr/src/app
-COPY package.json /usr/src/app/
-RUN npm install
-COPY . /usr/src/app
-EXPOSE 3000
-CMD [ 'npm', 'start' ]
-" >> Dockerfile
-
 DIRECTORY=UserManager
 
 if [ ! -d "$DIRECTORY" ]; then
   git clone git@github.com:BolajiOlajide/$DIRECTORY.git
 fi
 
-if type node > /dev/null
-  then
-    echo ""
-    echo "node is installed, skipping..."
-  else
-    brew install node
-    # sudo apt-get update
-    # sudo apt-get install nodejs
-    # sudo apt-get install npm
-  fi
-
 cd $DIRECTORY
 
-npm install
+rm Dockerfile
 
-npm start
+touch Dockerfile
+
+echo 'FROM node:latest
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+COPY package.json /usr/src/app/
+RUN npm install
+COPY . /usr/src/app
+EXPOSE 3000
+CMD [ "npm", "start" ]
+' >> Dockerfile
+
+rm docker-compose.yml
+
+touch docker-compose.yml
+
+echo 'version: "2"
+services:
+  app:
+    container_name: app
+    restart: always
+    build: .
+    ports:
+      - "3000:3000"
+    links:
+      - mongo
+    env_file:
+      - .env
+
+  mongo:
+    container_name: mongo
+    image: mongo
+    volumes:
+      - ./data:/data/db
+    ports:
+      - "27017:27017"
+' >> docker-compose.yml
+
+touch .env
+
+echo 'PORT=3000
+DB_URL=mongodb://localhost:27017/db
+' >> .env
+
+docker-compose up
